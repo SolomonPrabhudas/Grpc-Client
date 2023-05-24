e59e7b1738454113e75d8454b60cb73233192250
Solomon <solomonprabhudas123@gmail.com>
2023-05-24 16:22:07 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Grpc-Client" 
+"# Grpc-Client" 
--- src/main/java/com/example/demo/GrpcClient.java
+++ src/main/java/com/example/demo/GrpcClient.java
@@ -1,12 +1,12 @@
-package client;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.sample.javastubs.User.APIResponse;
-import com.sample.javastubs.User.LoginRequest;
-import com.sample.javastubs.userServiceGrpc;
-import com.sample.javastubs.userServiceGrpc.userServiceBlockingStub;
+import com.example.javastubs.userServiceGrpc;
+import com.example.javastubs.User.APIResponse;
+import com.example.javastubs.User.LoginRequest;
+import com.example.javastubs.userServiceGrpc.userServiceBlockingStub;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -14,14 +14,21 @@ import io.grpc.ManagedChannelBuilder;
 @SpringBootApplication
 public class GrpcClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		SpringApplication.run(GrpcClient.class, args);
 		
 		System.out.println("Grpc Client!!!");
 		
-		ManagedChannel channel = ManagedChannelBuilder
-				.forAddress("localhost", 8085).usePlaintext().build();
+		int i=1;
+		
+		ManagedChannel channel = null;
+		
+		while(i <= 10)
+		{
+			
+		channel = ManagedChannelBuilder
+				.forAddress("172.30.233.176", 8085).usePlaintext().build();
 		
 		userServiceBlockingStub userStub = userServiceGrpc.newBlockingStub(channel);
 		
@@ -32,7 +39,14 @@ public class GrpcClient {
 		
 		System.out.println(response.getResponsemessage());
 		
+		i++;
+		
+		Thread.sleep(10000);
+		
+		}
+		
 		channel.shutdown();
+		System.out.println("Task completed!!!");
 	}
 
 }
